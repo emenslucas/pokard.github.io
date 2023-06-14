9a4063ded44534a7876e21727d5dbb7c1431e331
emenslucas <emens.lucasp@gmail.com>
2023-06-14 11:37:34 -0300

    feat: delete img card after delete card

--- php/borrarItem.php
+++ php/borrarItem.php
@@ -14,15 +14,23 @@
 
 			if ($db_handle->connectDB()) {
 
-				// $con = mysqli_connect('localhost', 'root', '', 'pokard');
-
 				if (isset($_GET['idProducto'])) {
 					$idCarta = $_GET['idProducto'];
 					$nombreCarta = $_GET['nombreProducto'];
 
 					$consulta = "DELETE FROM carta WHERE id='$idCarta'";
 
+					$consultaImagen = "SELECT imagen FROM carta WHERE id='$idCarta'";
+					$resultadoImagen = mysqli_query($db_handle->connectDB(), $consultaImagen);
+					$filaImagen = mysqli_fetch_array($resultadoImagen);
+					$imagenCarta = $filaImagen['imagen'];
+
 					if ($resultado = mysqli_query($db_handle->connectDB(), $consulta)) {
+						$directorioImagen = "../img/cartas/";
+						$rutaImagen = $directorioImagen . $imagenCarta;
+						if (file_exists($rutaImagen)) {
+							unlink($rutaImagen);
+						}
 						echo "<h1>La carta $nombreCarta fue eliminada correctamente.</h1>";
 					  }
 				}
